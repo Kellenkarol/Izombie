712ca72510d7a4233728a8080e4a12c1bdbaf316
JHDsBR <Jesse2013.jsc@gmail.com>
2022-02-22 21:48:29 -0300

    Novos atributos
    adicionei opções para upgrade dos atributos

--- Brackeys Game Jam 2022.1/Assets/Scriptables/ScriptableOfCharacter.cs
+++ Brackeys Game Jam 2022.1/Assets/Scriptables/ScriptableOfCharacter.cs
@@ -12,25 +12,56 @@ public enum AttackTypes  {Melee, Shoot, Special}; // IMPORTANTE: o zumbi não te
 [CreateAssetMenu(fileName = "Character", menuName = "Character/New Character")]
 public class ScriptableOfCharacter : ScriptableObject
 {
+    [Tooltip("[Zombie] se já foi desbloqueado")]
+        public bool unlocked;
+
+
     // importante para salvamento de dados
-        public SpecieTypes specieType;
-        public GroupTypes groupType;
-        public AttackTypes attackType; 
+    [Header("Identification")]
+        [Tooltip("[Human/Zombie] Espécie")]                                                        public SpecieTypes specieType;
+        [Tooltip("[Human/Zombie] Tipo de grupo")]                                                  public GroupTypes groupType;
+        [Tooltip("[Human/Zombie] Tipo de ataque (Zumbi não tem o tipo 'Shoot')")]                  public AttackTypes attackType; 
 
 
-    public float damage;        // quanto causa de dano
-    public float speed;         // velocidade de movimento
-    public float attackDelay;   // tempo entre os ataques
+    [Tooltip("[Human] quantos humanos já devem ter sido mortos para começar a aparecer no mapa")]  public float humansNeededDie;        // [Human] quantos humanos já devem ter sido mortos para poder aparecer no mapa 
+
+
+    [Header("Damage")]
+        [Tooltip("[Human/Zombie] quanto causa de dano")]                                           public float damage;        // quanto causa de dano
+        [Tooltip("[Zombie] preço do upgrade do dano")]                                             public float damagePrice;        // preço do upgrade do dano
+        [Tooltip("[Zombie] o quanto o dano vai aumentar a cada upgrade")]                          public float damageMultiply;          // o quanto o dano vai aumentar a cada upgrade
+        [Tooltip("[Zombie] o quanto o preço do upgrade vai aumentar a cada compra")]               public float damageMultiplyPrice;          // o quanto o preço do upgrade vai aumentar a cada compra
+        [Tooltip("[Zombie] o máximo que o dano pode chegar (-1 = infinito)")]                      public float damageMax;          // o quanto o preço do upgrade vai aumentar a cada compra
+    
+
+    [Header("Speed")]
+        [Tooltip("[Human/Zombie] velocidade de movimento")]                                        public float speed;        // velocidade de movimento
+        [Tooltip("[Zombie] preço do upgrade")]                                                     public float speedPrice;        // preço do upgrade do dano
+        [Tooltip("[Zombie] o quanto a velocidade vai aumentar a cada upgrade")]                    public float speedMultiply;          // o quanto o dano vai aumentar a cada upgrade
+        [Tooltip("[Zombie] o quanto o preço do upgrade vai aumentar a cada compra")]               public float speedMultiplyPrice;          // o quanto o preço do upgrade vai aumentar a cada compra
+        [Tooltip("[Zombie] o máximo que a velocidade pode chegar (-1 = infinito)")]                public float speedMax;          // o quanto o preço do upgrade vai aumentar a cada compra
+
+
+    [Header("AttackDelay")]
+        [Tooltip("[Human/Zombie] tempo entre os ataques")]                                         public float attackDelay;        // tempo entre os ataques
+        [Tooltip("[Zombie] preço do upgrade")]                                                     public float attackDelayPrice;        // preço do upgrade
+        [Tooltip("[Zombie] o quanto o tempo vai diminuir a cada upgrade")]                         public float attackDelayMultiply;          // o quanto o tempo vai diminuir a cada upgrade
+        [Tooltip("[Zombie] o quanto o preço do upgrade vai aumentar a cada compra")]               public float attackDelayMultiplyPrice;          // o quanto o preço do upgrade vai aumentar a cada compra
+        [Tooltip("[Zombie] o mínimo que o tempo pode chegar (precisa ser maior ou igual a 0)")]    public float attackDelayMax;          // o quanto o preço do upgrade vai aumentar a cada compra
+    
+    
+
 
 
     // retorna todos os dados do tipo float de uma forma genérica (ótimo para upgrades na lojinha)
-    public Dictionary<string, float> GetFloatProperties()
+    public Dictionary<string, List<float>> GetFloatProperties()
     {
-        return new Dictionary<string, float>()
+        // OBS: Não remover uma propriedade depois de adicionar
+        return new Dictionary<string, List<float>>()
         {
-            {"damage",          damage},
-            {"speed",           speed},
-            {"attackDelay",     attackDelay},
+            {"damage",          new List<float>{damage,      damagePrice,      damageMultiply,      damageMultiplyPrice,      damageMax}},
+            {"speed",           new List<float>{speed,       speedPrice,       speedMultiply,       speedMultiplyPrice,       speedMax}},
+            {"attackDelay",     new List<float>{attackDelay, attackDelayPrice, attackDelayMultiply, attackDelayMultiplyPrice, attackDelayMax}},
             
             //{"", },
             //...
